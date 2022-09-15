84d38d9890517a3eeacea74275df2889dba1b6ad
LayaboutSi <110651062+LayaboutSi@users.noreply.github.com>
2022-09-15 14:46:35 +1000

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "";
 const KOVAN_RPC_URL = process.env.KOVAN_RPC_URL || "https://eth-mainnet.alchemyapi.io/v2/your-api-key";
 const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || "https://eth-mainnet.alchemyapi.io/v2/your-api-key";
-const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a";
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL || "";
 
